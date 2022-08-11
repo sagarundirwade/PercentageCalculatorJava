7c43bf88d949ca1ae5c8fe67ce7505b7c8f7b5a9
Sagar Undirwade <109235561+sagarundirwade@users.noreply.github.com>
2022-08-12 00:09:02 +0530

    Add files via upload

---
+++ mycode1.java
@@ -0,0 +1,181 @@
+import java.util.Scanner;
+
+public class mycode1 {
+
+    public static void main(String args[]) {
+        Scanner cs = new Scanner(System.in);
+        System.out.print(
+                " Subjech 5 \n Subject 6 \n Subject 7 \n Subject 8 \n Subject 9 \n Subject 10 \n Enter your Number of Subjects : ");
+        // System.out.print();
+        long a = cs.nextLong();
+        // also use this
+        // int a = cs.nextLong();
+        // etc if you like
+
+        if (a == 5) {
+            // text code
+            // System.out.println("555555555");
+            // text code
+
+            // this code run
+            System.out.print("Enter Your First(1) subject Marks : ");
+            float sub0 = cs.nextFloat();
+            System.out.print("Enter Your Second(2) subject Marks : ");
+            float sub1 = cs.nextFloat();
+            System.out.print("Enter Your Third(3) subject Marks : ");
+            float sub2 = cs.nextFloat();
+            System.out.print("Enter Your Forth(4) subject Marks : ");
+            float sub3 = cs.nextFloat();
+            System.out.print("Enter Your Forth(5) subject Marks : ");
+            float sub4 = cs.nextFloat();
+            // System.out.print("Enter Your Fifth(5) subject Marks : ");
+
+            float sum0 = sub0 + sub1 + sub2 + sub3 + sub4;
+            float sum1 = sum0 / 500 * 100;
+            System.out.println(sum1 + "%");
+
+        } else if (a == 6) {
+            // text code
+            // System.out.println("6666666666");
+            // text code
+
+            System.out.print("Enter Your First(1) subject Marks : ");
+            float sub0 = cs.nextFloat();
+            System.out.print("Enter Your Second(2) subject Marks : ");
+            float sub1 = cs.nextFloat();
+            System.out.print("Enter Your Third(3) subject Marks : ");
+            float sub2 = cs.nextFloat();
+            System.out.print("Enter Your Forth(4) subject Marks : ");
+            float sub3 = cs.nextFloat();
+            System.out.print("Enter Your Fifth(5) subject Marks : ");
+            float sub4 = cs.nextFloat();
+            System.out.print("Enter Your Sixth(6) subject Marks : ");
+            float sub5 = cs.nextFloat();
+
+            // float sum2 = sum0 + sub5; <<<============== why nt work this code /// sryyy
+            // my mistek
+            // System.out.prinntln(sum0); <<<============== why not use this sum0 /// sryyy
+            // my mistek
+
+            float sum0 = sub0 + sub1 + sub2 + sub3 + sub4 + sub5;
+            float sum1 = sum0 / 600 * 100;
+            System.out.println(sum1 + "%");
+
+        } else if (a == 7) {
+            // text code
+            // System.out.println("7777777777777");
+            // text code
+
+            System.out.print("Enter Your First(1) subject Marks : ");
+            float sub0 = cs.nextFloat();
+            System.out.print("Enter Your Second(2) subject Marks : ");
+            float sub1 = cs.nextFloat();
+            System.out.print("Enter Your Third(3) subject Marks : ");
+            float sub2 = cs.nextFloat();
+            System.out.print("Enter Your Forth(4) subject Marks : ");
+            float sub3 = cs.nextFloat();
+            System.out.print("Enter Your Fifth(5) subject Marks : ");
+            float sub4 = cs.nextFloat();
+            System.out.print("Enter Your Sixth(6) subject Marks : ");
+            float sub5 = cs.nextFloat();
+            System.out.print("Enter Your Seventh(7) subject Marks : ");
+            float sub6 = cs.nextFloat();
+
+            float sum0 = sub0 + sub1 + sub2 + sub3 + sub4 + sub5 + sub6;
+            float sum1 = sum0 / 700 * 100;
+
+            System.out.println(sum1 + "%");
+
+        } else if (a == 8) {
+            // text code
+            // System.out.println("8888888888");
+            // text code
+
+            System.out.print("Enter Your First(1) subject Marks : ");
+            float sub0 = cs.nextFloat();
+            System.out.print("Enter Your Second(2) subject Marks : ");
+            float sub1 = cs.nextFloat();
+            System.out.print("Enter Your Third(3) subject Marks : ");
+            float sub2 = cs.nextFloat();
+            System.out.print("Enter Your Forth(4) subject Marks : ");
+            float sub3 = cs.nextFloat();
+            System.out.print("Enter Your Fifth(5) subject Marks : ");
+            float sub4 = cs.nextFloat();
+            System.out.print("Enter Your Sixth(6) subject Marks : ");
+            float sub5 = cs.nextFloat();
+            System.out.print("Enter Your Seventh(7) subject Marks : ");
+            float sub6 = cs.nextFloat();
+            System.out.print("Enter Your Eighth(8) subject Marks : ");
+            float sub7 = cs.nextFloat();
+
+            float sum0 = sub0 + sub1 + sub2 + sub3 + sub4 + sub5 + sub6 + sub7;
+            float sum1 = sum0 / 800 * 100;
+
+            System.out.println(sum1 + "%");
+
+        } else if (a == 9) {
+            // text code
+            // System.out.println("999999999999999");
+            // text code
+
+            System.out.print("Enter Your First(1) subject Marks : ");
+            float sub0 = cs.nextFloat();
+            System.out.print("Enter Your Second(2) subject Marks : ");
+            float sub1 = cs.nextFloat();
+            System.out.print("Enter Your Third(3) subject Marks : ");
+            float sub2 = cs.nextFloat();
+            System.out.print("Enter Your Forth(4) subject Marks : ");
+            float sub3 = cs.nextFloat();
+            System.out.print("Enter Your Fifth(5) subject Marks : ");
+            float sub4 = cs.nextFloat();
+            System.out.print("Enter Your Sixth(6) subject Marks : ");
+            float sub5 = cs.nextFloat();
+            System.out.print("Enter Your Seventh(7) subject Marks : ");
+            float sub6 = cs.nextFloat();
+            System.out.print("Enter Your Eighth(8) subject Marks : ");
+            float sub7 = cs.nextFloat();
+            System.out.print("Enter Your ninth(9) subject Marks : ");
+            float sub8 = cs.nextFloat();
+
+            float sum0 = sub0 + sub1 + sub2 + sub3 + sub4 + sub5 + sub6 + sub7 + sub8;
+            float sum1 = sum0 / 900 * 100;
+
+            System.out.println(sum1 + "%");
+
+        } else if (a == 10) {
+            // text code
+            // System.out.println("10101010101010");
+            // text code
+
+            System.out.print("Enter Your First(1) subject Marks : ");
+            float sub0 = cs.nextFloat();
+            System.out.print("Enter Your Second(2) subject Marks : ");
+            float sub1 = cs.nextFloat();
+            System.out.print("Enter Your Third(3) subject Marks : ");
+            float sub2 = cs.nextFloat();
+            System.out.print("Enter Your Forth(4) subject Marks : ");
+            float sub3 = cs.nextFloat();
+            System.out.print("Enter Your Fifth(5) subject Marks : ");
+            float sub4 = cs.nextFloat();
+            System.out.print("Enter Your Sixth(6) subject Marks : ");
+            float sub5 = cs.nextFloat();
+            System.out.print("Enter Your Seventh(7) subject Marks : ");
+            float sub6 = cs.nextFloat();
+            System.out.print("Enter Your Eighth(8) subject Marks : ");
+            float sub7 = cs.nextFloat();
+            System.out.print("Enter Your ninth(9) subject Marks : ");
+            float sub8 = cs.nextFloat();
+            System.out.print("Enter Your tenth(10) subject Marks : ");
+            float sub9 = cs.nextFloat();
+
+            float sum0 = sub0 + sub1 + sub2 + sub3 + sub4 + sub5 + sub6 + sub7 + sub8 + sub9;
+            float sum1 = sum0 / 1000 * 100;
+
+            System.out.println(sum1 + "%");
+        } else {
+            System.out.println("invalid subject");
+        }
+
+    }
+
+}
